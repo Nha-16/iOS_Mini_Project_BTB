61884cf89d122a399e15ed2950cd6b2946156c36
Panha <yoeunsopanha2001@gmail.com>
2021-11-28 17:01:19 +0700

    commit UI

---
+++ iOS_Mini_Project_Panha_BTB/Application/Profile/Controller/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  iOS_Mini_Project_Panha_BTB
+//
+//  Created by BTB_001 on 28/11/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
